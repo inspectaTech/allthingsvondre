9567a9fb8e304154025ad96de923edfac36169f8
inspectaTech <d3po.techengineer@gmail.com>
2024-04-02 16:21:08 -0400

    test html cdn with dist

---
+++ dist/assets/templates/DelayDefer/jsx-CnKvf75Y.js
@@ -0,0 +1,1 @@
+import{j as e}from"../../vendor-DgJEq-XA.js";const s=()=>(console.group("Vite"),console.log("[DelayDefer] accessed"),console.groupEnd(),e.jsx("span",{children:"What"}));export{s as default};
---
+++ dist/assets/templates/MegaOneClsDefer/jsx-MGH-kMbO.js
@@ -0,0 +1,1 @@
+import{r as e}from"../../vendor-DgJEq-XA.js";import"../../common-EjicN4om.js";const o=({callback:r})=>(e.useEffect(()=>(r&&r(),()=>{}),[]),null);export{o as default};
